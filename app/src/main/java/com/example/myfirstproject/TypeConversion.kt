d7924383789ce03e4ac3eea42b8721279a23db09
Vinh Nguyen <ntanvinh@tma.com.vn>
2024-06-17 23:07:35 +0700

    17. Type Conversions in Kotlin

--- app/src/main/java/com/example/myfirstproject/TypeConversion.kt
+++ app/src/main/java/com/example/myfirstproject/TypeConversion.kt
@@ -1,2 +1,28 @@
 package com.example.myfirstproject
 
+/*
+toByte()
+toShort()
+toInt()
+toLong()
+toFloat()
+toDouble()
+ */
+
+fun main(args: Array<String>) {
+//    var x:Byte = 127
+//    var y:Int = x.toInt()
+//    var z:Double = y.toDouble()
+//
+//    println("Byte: $x")
+//    println("Int: $y")
+//    println("Double: $z")
+
+    var a:Double = 132.32
+    var b:Int = a.toInt()
+    var c:Byte = b.toByte()
+
+    println(a)
+    println(b)
+    println(c)
+}
