defdd276473ee5a4278565ed4d685e6cb80a6590
Vinh Nguyen <ntanvinh@tma.com.vn>
2024-06-17 22:48:25 +0700

    15. Variables in Kotlin

---
+++ app/src/main/java/com/example/myfirstproject/Variables.kt
@@ -0,0 +1,31 @@
+package com.example.myfirstproject
+
+fun main(args: Array<String>) {
+    /*
+    var a = 10
+    var b = 20
+    var c = a + b
+
+    println("The sum of $a and $b is $c")
+
+    var age = 10 // valid
+
+    // This is a single comment line
+    /* This is a multi-line comment
+    and it can span multiple lines
+     */
+
+    var _age = 20 // valid
+    var age3 = 30 // valid
+
+    var a ge = 10 // invalid
+    var 3age = 20 // invalid
+    */
+
+    var age = 20
+    age = 10
+    println("The age is $age")
+
+    val year = 2000
+//    year = 2001
+}
