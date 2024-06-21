d3b3393693d71c28ee151d93330ebc65bc2244a5
Vinh Nguyen <ntanvinh@tma.com.vn>
2024-06-21 17:07:39 +0700

    21. Set

---
+++ app/src/main/java/com/example/myfirstproject/CollectionsSet.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstproject
+
+fun main(args: Array<String>) {
+//    var mySetArray = arrayListOf<Any>("BMW", 25, 25.5, true, "BMW")
+//    println("Size of mySetArray: " + mySetArray.size)
+//    println("Last Element: " + mySetArray.last())
+
+    var mySetArray = setOf<Any>("BMW", 25, 25.5, true, "BMW")
+    println("Size of mySetArray: " + mySetArray.size)
+    println("Last Element: " + mySetArray.last())
+
+}
