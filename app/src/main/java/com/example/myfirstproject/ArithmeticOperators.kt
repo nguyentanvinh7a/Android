99b4ab930a70547b780248cac4f02d16b78d25b0
Vinh Nguyen <ntanvinh@tma.com.vn>
2024-06-23 17:46:16 +0700

    Session 4: Operators in Kotlin

---
+++ app/src/main/java/com/example/myfirstproject/ArithmeticOperators.kt
@@ -0,0 +1,27 @@
+package com.example.myfirstproject
+
+fun main(args: Array<String>) {
+    var number1:Double = 12.5
+    var number2:Double = 3.5
+    var result:Double = 0.0
+
+    // Addition
+    result = number1 + number2
+    println("number1 + number2 = $result")
+
+    // Subtraction
+    result = number1 - number2
+    println("number1 - number2 = $result")
+
+    // Multiplication
+    result = number1 * number2
+    println("number1 * number2 = $result")
+
+    // Division
+    result = number1 / number2
+    println("number1 / number2 = $result")
+
+    // Modulus
+    result = number1 % number2
+    println("number1 % number2 = $result")
+}
